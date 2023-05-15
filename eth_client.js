5e448bb19b1f18663823642f7efd575a30b17093
chetankashetti <chetan@unmarshal.io>
2023-05-15 22:36:32 +0530

    feat(geth): added rpc read methods to get block, transaction and log details

---
+++ eth_client.js
@@ -0,0 +1,45 @@
+const Web3 = require('web3');
+
+// create a web3 instance
+const web3 = new Web3('https://nd-726-055-946.p2pify.com/9f786b449e03850052796efffe90d787');
+
+
+// Define a function to retrieve a block by block number with retries in case of failure
+async function getBlockWithRetries(blockNumber, maxRetries) {
+    let retries = 0;
+    while (retries < maxRetries) {
+        try {
+            const block = await web3.eth.getBlock(blockNumber, true);
+            return block;
+        } catch (error) {
+            console.error(`Error retrieving block ${blockNumber}. Retrying...`);
+            retries++;
+        }
+    }
+    console.error(`Max retries reached for block ${blockNumber}. Aborting...`);
+    return null;
+}
+
+
+async function getTransactionReceiptsWithRetries(block) {
+    let receipts = [];
+    let retries = 0; 
+    let maxRetries = 3;
+    for (let i = 0; i < block.transactions.length; i++) {
+            try {
+                let hash = block.transactions[i].hash;
+                const receipt = await web3.eth.getTransactionReceipt(hash);
+                if (receipt != null) {
+                    receipts.push(receipt);
+                }
+                console.log(`fetched ${hash}`);
+            } catch (error) {
+                console.error(`Error retrieving receipt ${block.transactions[i].transactionHash}. Retrying...`);
+            }
+    }
+    console.log(receipts);
+    return receipts;
+}
+
+
+module.exports = { getBlockWithRetries, getTransactionReceiptsWithRetries };
